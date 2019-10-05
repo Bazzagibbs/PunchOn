ce2002b9872283040e609d671bf9d5383fbd7149
Bailey Gibbons <bailey.gibbons2018@gmail.com>
2019-10-05 18:14:05 +1000

    Working on player sprites, started on environment sprites

---
+++ Assets/Controller2D.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent (typeof(Animator))]
+
+public class Controller2D : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 5f;
+    [SerializeField] private LayerMask groundMask = -1;
+    [SerializeField] private float smoothing = 0.05f;
+    [SerializeField] private Rect groundCheckCollider = new Rect(0f, -1f, .5f, .2f);
+
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+    private bool onGround = true;
+    private bool moving = false;
+    private Animator animator;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
